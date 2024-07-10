f6cfc1aaf38bdf33f9602e4bcf84583d6406dca2
Platon61633 <peredreevplaton@gmail.com>
2024-07-11 00:10:15 +0300

    fghj

--- api/index.php
+++ api/index.php
@@ -6,6 +6,11 @@
 echo 'e';
 require_once __DIR__.'/configuration/connect.php';
 $method = $_SERVER['REQUEST_METHOD'];
+if ($connect) {
+    echo 'gg';
+} else {
+    echo 'bad';
+}
 
 echo $method; 
 
